d34b28caf338835ad7cfbd3404ab78738c5d772f
Francesco Ballarin <francesco.ballarin@unicatt.it>
2022-07-18 09:34:20 -0400

    dolfinx.io module now allows to load mesh from .gmsh file

--- tutorials/01_introduction/utils/__init__.py
+++ tutorials/01_introduction/utils/__init__.py
@@ -13,11 +13,3 @@ except ImportError:
 else:
     from .gmsh_to_fenics import gmsh_to_fenics
     __all__ += ["gmsh_to_fenics"]
-
-try:
-    import dolfinx  # noqa: F401
-except ImportError:
-    pass
-else:
-    from .gmsh_to_fenicsx import gmsh_to_fenicsx
-    __all__ += ["gmsh_to_fenicsx"]
