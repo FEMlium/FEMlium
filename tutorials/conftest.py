d449a3f961a7e39ecf7fdb47e5a3ab5a9d118fb9
Francesco Ballarin <francesco.ballarin@unicatt.it>
2022-08-18 15:41:00 +0200

    Use nbvalx conftest for tutorials

--- tutorials/conftest.py
+++ tutorials/conftest.py
@@ -5,103 +5,50 @@
 # SPDX-License-Identifier: MIT
 """pytest configuration file for tutorials tests."""
 
+import fnmatch
 import os
-import pathlib
-import sys
-import typing
+import shutil
 
-import nbconvert.preprocessors
-import nbformat
-import py
+import _pytest
+import nbvalx.pytest_hooks_notebooks
 import pytest
 
+pytest_addoption = nbvalx.pytest_hooks_notebooks.addoption
+pytest_sessionstart = nbvalx.pytest_hooks_notebooks.sessionstart
+pytest_collect_file = nbvalx.pytest_hooks_notebooks.collect_file
+pytest_runtest_makereport = nbvalx.pytest_hooks_notebooks.runtest_makereport
+pytest_runtest_teardown = nbvalx.pytest_hooks_notebooks.runtest_teardown
 
-def pytest_ignore_collect(collection_path: pathlib.Path, path: py.path.local, config: pytest.Config) -> bool:
-    """Ignore .py files obtained from previous runs."""
-    if collection_path.suffix == ".py" and collection_path.with_suffix(".ipynb").exists():
-        return True
+
+def pytest_runtest_setup(item: pytest.File) -> None:
+    """Copy data files to destination folder, and check backend availability."""
+    # Do the setup as in nbvalx
+    nbvalx.pytest_hooks_notebooks.runtest_setup(item)
+    # Get notebook name
+    notebook_name = item.parent.name
+    # Copy data files
+    if item.name == "Cell 0":
+        work_dir = item.parent.config.option.work_dir
+        notebook_original_dir = os.path.dirname(notebook_name).replace(work_dir, "")
+        for dir_entry in _pytest.pathlib.visit(notebook_original_dir, lambda _: True):
+            if dir_entry.is_file():
+                source_path = str(dir_entry.path)
+                if fnmatch.fnmatch(source_path, "**/*.csv") or fnmatch.fnmatch(source_path, "**/*.msh"):
+                    destination_path = os.path.join(
+                        notebook_original_dir, work_dir, os.path.relpath(source_path, notebook_original_dir))
+                    if not os.path.exists(destination_path):
+                        os.makedirs(os.path.dirname(destination_path), exist_ok=True)
+                        shutil.copyfile(source_path, destination_path)
+    # Check backend availability depending on the item name
+    if notebook_name.endswith("dolfin.ipynb"):
+        pytest.importorskip("dolfin")
+    elif notebook_name.endswith("dolfinx.ipynb"):
+        pytest.importorskip("dolfinx")
+    elif notebook_name.endswith("firedrake.ipynb"):
+        pytest.importorskip("firedrake")
+    elif notebook_name.endswith("meshio.ipynb"):
+        pytest.importorskip("meshio")
+    elif notebook_name.endswith("generate_mesh.ipynb"):
+        pass
     else:
-        return False
-
-
-def pytest_collect_file(
-    file_path: pathlib.Path, path: py.path.local, parent: pytest.Collector
-) -> typing.Optional[pytest.File]:
-    """Collect tutorial files."""
-    if file_path.suffix == ".ipynb":
-        if not file_path.name.startswith("x"):
-            return TutorialFile.from_parent(parent=parent, path=file_path)
-        else:
-            return DoNothingFile.from_parent(parent=parent, path=file_path)
-    elif file_path.suffix == ".py":
-        assert not file_path.with_suffix(".ipynb").exists(), (
-            "Please run pytest on jupyter notebooks, not plain python files.")
-        return DoNothingFile.from_parent(parent=parent, path=file_path)
-
-
-def pytest_pycollect_makemodule(
-    module_path: pathlib.Path, path: py.path.local, parent: pytest.Collector
-) -> typing.Optional[pytest.Module]:
-    """Disable running .py files produced by previous runs."""
-    if module_path.suffix == ".py":
-        assert not module_path.with_suffix(".ipynb").exists(), (
-            "Please run pytest on jupyter notebooks, not plain python files.")
-        return DoNothingFile.from_parent(parent=parent, path=module_path)
-
-
-class TutorialFile(pytest.File):
-    """Custom file handler for tutorial files."""
-
-    def collect(self) -> typing.Iterable[pytest.Item]:
-        """Collect tutorial file."""
-        yield TutorialItem.from_parent(
-            parent=self, name="run_tutorial -> " + os.path.relpath(str(self.path), str(self.parent.path)))
-
-
-class TutorialItem(pytest.Item):
-    """Handle the execution of the tutorial."""
-
-    def __init__(self, name: str, parent: TutorialFile) -> None:
-        """Initialize tutorial item."""
-        super(TutorialItem, self).__init__(name, parent)
-
-    def runtest(self) -> None:
-        """Run tutorial."""
-        self._import_backend_or_skip()
-        os.chdir(self.parent.path.parent)
-        sys.path.append(str(self.parent.path.parent))
-        with open(self.parent.path) as f:
-            nb = nbformat.read(f, as_version=4)
-        execute_preprocessor = nbconvert.preprocessors.ExecutePreprocessor()
-        try:
-            execute_preprocessor.preprocess(nb)
-        finally:
-            with open(self.parent.path, "w") as f:
-                nbformat.write(nb, f)
-
-    def _import_backend_or_skip(self) -> None:
-        """Check if backend is available, or skip."""
-        if self.name.endswith("dolfin.ipynb"):
-            pytest.importorskip("dolfin")
-        elif self.name.endswith("dolfinx.ipynb"):
-            pytest.importorskip("dolfinx")
-        elif self.name.endswith("firedrake.ipynb"):
-            pytest.importorskip("firedrake")
-        elif self.name.endswith("meshio.ipynb"):
-            pytest.importorskip("meshio")
-        elif self.name.endswith("generate_mesh.ipynb"):
-            pass
-        else:
-            raise ValueError("Invalid name " + self.name)
-
-    def reportinfo(self) -> typing.Tuple[pathlib.Path, int, str]:
-        """Info for test report."""
-        return self.path, 0, self.name
-
-
-class DoNothingFile(pytest.File):
-    """Custom file handler to avoid running twice python files explicitly provided on the command line."""
-
-    def collect(self) -> typing.List[pytest.Item]:
-        """Mock collect."""
-        return []
+        raise ValueError("Invalid notebook name " + notebook_name)
