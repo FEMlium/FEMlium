927ba8b5d4da4a6077ad375ffe092e425356fe63
Francesco Ballarin <francesco.ballarin@unicatt.it>
2024-01-02 21:12:23 +0100

    nbvalx now allows to pass glob patterns of data files to be copied while testing

--- tutorials/conftest.py
+++ tutorials/conftest.py
@@ -5,11 +5,6 @@
 # SPDX-License-Identifier: MIT
 """pytest configuration file for tutorials tests."""
 
-import fnmatch
-import os
-import shutil
-
-import _pytest
 import nbvalx.pytest_hooks_notebooks
 import pytest
 
@@ -20,29 +15,22 @@ pytest_runtest_makereport = nbvalx.pytest_hooks_notebooks.runtest_makereport
 pytest_runtest_teardown = nbvalx.pytest_hooks_notebooks.runtest_teardown
 
 
+def pytest_sessionstart(session: pytest.Session) -> None:
+    """Automatically mark mesh files as data to be copied to the work directory."""
+    # Add mesh files as data to be copied
+    copy_data_to_work_dir = session.config.option.copy_data_to_work_dir
+    assert len(copy_data_to_work_dir) == 0
+    copy_data_to_work_dir.extend(["**/*.csv", "**/*.msh"])
+    # Start session as in nbvalx
+    nbvalx.pytest_hooks_notebooks.sessionstart(session)
+
+
 def pytest_runtest_setup(item: pytest.File) -> None:
     """Copy data files to destination folder, and check backend availability."""
     # Do the setup as in nbvalx
     nbvalx.pytest_hooks_notebooks.runtest_setup(item)
     # Get notebook name
     notebook_name = item.parent.name
-    # Copy data files
-    if item.name == "Cell 0":
-        work_dir = item.parent.config.option.work_dir
-        notebook_original_dir = os.path.dirname(notebook_name).replace(work_dir, "")
-        for dir_entry in _pytest.pathlib.visit(notebook_original_dir, lambda _: True):
-            if dir_entry.is_file():
-                source_path = str(dir_entry.path)
-                if (
-                    (fnmatch.fnmatch(source_path, "**/*.csv") or fnmatch.fnmatch(source_path, "**/*.msh"))
-                        and
-                    work_dir not in source_path
-                ):
-                    destination_path = os.path.join(
-                        notebook_original_dir, work_dir, os.path.relpath(source_path, notebook_original_dir))
-                    if not os.path.exists(destination_path):
-                        os.makedirs(os.path.dirname(destination_path), exist_ok=True)
-                        shutil.copyfile(source_path, destination_path)
     # Check backend availability depending on the item name
     if notebook_name.endswith("dolfin.ipynb"):
         pytest.importorskip("dolfin")
